3e4ba895e8dc0976fd595ef46291a42ec704eda6
Russell Gordon <rgordon@lcs.on.ca>
2023-04-20 14:50:17 -0400

    Added example JSON response.

--- PoetryFinder.playground/Contents.swift
+++ PoetryFinder.playground/Contents.swift
@@ -1,5 +1,37 @@
 import SwiftUI
 
+// Example JSON response
+//
+// From endpoint: https://poetrydb.org/random/1
+//
+let exampleResponse = """
+ 
+ [
+   {
+     "title": "Sonnet XLIV: Press'd by the Moon",
+     "author": "Charlotte Smith",
+     "lines": [
+       "Press'd by the Moon, mute arbitress of tides,",
+       "While the loud equinox its power combines,",
+       "The sea no more its swelling surge confines,",
+       "But o'er the shrinking land sublimely rides.",
+       "The wild blast, rising from the Western cave,",
+       "Drives the huge billows from their heaving bed;",
+       "Tears from their grassy tombs the village dead,",
+       "And breaks the silent sabbath of the grave!",
+       "With shells and sea-weed mingled, on the shore",
+       "Lo! their bones whiten in the frequent wave;",
+       "But vain to them the winds and waters rave;",
+       "They hear the warring elements no more:",
+       "While I am doom'dâ€”by life's long storm opprest,",
+       "To gaze with envy on their gloomy rest."
+     ],
+     "linecount": "14"
+   }
+ ]
+
+"""
+
 // Model
 //
 //
@@ -38,7 +70,7 @@ func fetch() async -> Poem? {
         // Create a decoder object to do most of the work for us
         let decoder = JSONDecoder()
         
-        // Use the decoder object to convert the raw data into an instance of our Swift data type
+        // Use the decoder object to convert the raw data into an array of the Poem data type
         let decodedData = try decoder.decode([Poem].self, from: data)
 
         // If we got here, we have received an array of poems
